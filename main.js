97db4c6b7a1f4503f32a890cd80635b0b3cea5c6
Sébastien Guimmara <sguimmara@pobox.com>
2023-04-24 22:01:56 +0200

    recherche

--- main.js
+++ main.js
@@ -61,12 +61,14 @@ const highlightIcon = f => new Style({
     })
 });
 
-const cities = new VectorLayer({
+const featureLayer = new VectorLayer({
     source: new VectorSource(),
 })
 
+const source = featureLayer.getSource();
+
 const map = new Map({
-    layers: [basemap, cities],
+    layers: [basemap, featureLayer],
     target: 'map',
     view: new View({
         center: fromLonLat([139.340, 38.822]),
@@ -95,19 +97,93 @@ map.on('click', (evt) => {
     }
 });
 
-function jour(num, lat, lon, url) {
+const features = Array(90);
+
+function parseDate(text) {
+    const [day, month, year] = text.split('/').map(x => Number.parseInt(x));
+
+    return new Date(year, month - 1, day);
+}
+
+function jour(num, lat, lon, date, url, ...tags) {
     const feature = new Feature({
         geometry: new Point(fromLonLat([lon, lat])),
-        name: 'jour ' + num,
+        name: `jour ${num}`,
+        tags,
+        day: num,
+        date: parseDate(date),
         url,
     });
 
     feature.setStyle(defaultIcon);
 
-    cities.getSource().addFeature(feature);
+    features[num] = feature;
+    featureLayer.getSource().addFeature(feature);
 }
 
-jour(1, 35.553333, 139.781111, 'https://www.instagram.com/p/CjcrM7BLydo')
-jour(2, 35.6715, 139.7357, 'https://www.instagram.com/p/CjfTkwhL4U8/');
-jour(3, 35.670761980741965, 139.70809949014006, 'https://www.instagram.com/p/Cjh3Zo7LO1a/');
-jour(4, 35.72924969846609, 139.71942861764683, 'https://www.instagram.com/p/Cjkf3uErKfK/');
+function clearFeatures() {
+    source.clear();
+}
+
+window.search = function (params) {
+    clearFeatures();
+
+    if (params === "") {
+        source.addFeatures(features);
+        return;
+    }
+
+    const keywords = params.split(',').map(w => w.trim());
+
+    function test(feature) {
+        const name = feature.get('name');
+        if (keywords.some(kw => name.includes(kw))) {
+            return true;
+        }
+
+        const tags = feature.get('tags');
+        if (tags.some(t => keywords.some(kw => t.includes(kw)))) {
+            return true;
+        }
+
+        return false;
+    }
+
+    for (const feature of features) {
+        if (test(feature)) {
+            source.addFeature(feature);
+        }
+    }
+}
+
+function addDate(num) {
+    if (num < 0) {
+        return;
+    }
+
+    if (num >= features.length) {
+        return;
+    }
+
+    const f = features[num];
+    if (f) {
+        source.addFeature(f);
+    }
+}
+
+window.setDate = function (txt) {
+    clearFeatures();
+
+    const date = Number.parseInt(txt);
+    addDate(date - 1);
+    addDate(date);
+    addDate(date + 1);
+}
+
+jour(1, 35.553, 139.781, '22/09/2022', 'https://www.instagram.com/p/CjcrM7BLydo', 'arrivée', 'aéroport');
+jour(2, 35.671, 139.735, '24/09/2022', 'https://www.instagram.com/p/CjfTkwhL4U8/', 'pluie', 'fatigue', 'tokyo', 'salade', 'cuisine');
+jour(3, 35.670, 139.708, '25/09/2022', 'https://www.instagram.com/p/Cjh3Zo7LO1a/', 'tokyo', 'harajuku', 'design', 'fiesta', 'gallery');
+jour(4, 35.729, 139.719, '26/09/2022', 'https://www.instagram.com/p/Cjkf3uErKfK/', 'tokyo', 'kiddy', 'land', 'ikea');
+
+jour(65, 33.595, 130.403, '26/11/2022', 'https://www.instagram.com/p/CnxUrR4LcyI/', 'fukuoka', 'kyushu', 'sanctuaire', 'dazaifu', 'tenman-gu', 'tenman', 'ramen', 'croquette');
+jour(79, 42.988, 142.400, '10/12/2022', 'https://www.instagram.com/p/CpYEiNhNI--/', 'hokkaido', 'shimukappu', 'neige', 'restaurant', 'curry', 'train');
