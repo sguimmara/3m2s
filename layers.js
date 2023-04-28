b0d192271ad1274b7257a973798585b3b2f7d903
Sébastien Guimmara <sguimmara@pobox.com>
2023-04-28 22:23:59 +0200

    refactor

---
+++ layers.js
@@ -0,0 +1,59 @@
+import VectorLayer from "ol/layer/Vector";
+import VectorSource from "ol/source/Vector";
+import GeoJSON from "ol/format/GeoJSON";
+import TileLayer from 'ol/layer/Tile.js';
+import OSM from 'ol/source/OSM.js';
+import Stamen from 'ol/source/Stamen.js';
+
+import styles from './styles';
+
+function loadBasemaps() {
+    const transition = 16;
+
+    const watercolor = new TileLayer({
+        source: new Stamen({ layer: 'watercolor' }),
+        maxZoom: transition,
+    });
+
+    const osm = new TileLayer({
+        source: new OSM({
+            url: 'https://a.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png',
+        }),
+        minZoom: transition,
+    });
+
+    return [watercolor, osm];
+}
+
+/**
+ * Loads a GeoJSON and returns the created layer.
+ * @param {string} url The URL to the GeoJSON file.
+ * @returns {VectorLayer} The created vector layer.
+ */
+async function loadGeoJSON(url) {
+    const res = await fetch(url);
+    const json = await res.json();
+
+    const format = new GeoJSON({
+        featureProjection: 'EPSG:3857'
+    });
+
+    const features = format.readFeatures(json);
+
+    features.forEach(f => f.set('state', 'default'));
+
+    const layer = new VectorLayer({
+        source: new VectorSource({
+            attributions: 'Morgane Hamon',
+            features,
+        }),
+        style: f => styles[f.get('state')]
+    });
+
+    return layer;
+}
+
+export {
+    loadGeoJSON,
+    loadBasemaps,
+}
--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 import _ from 'lodash';
 
 import Map from 'ol/Map.js';
+import GeoJSON from 'ol/format/GeoJSON';
 import View from 'ol/View.js';
 import { easeOut } from 'ol/easing.js';
 import { fromLonLat } from 'ol/proj';
@@ -17,72 +18,41 @@ import VectorSource from 'ol/source/Vector';
 import Style from 'ol/style/Style';
 import Icon from 'ol/style/Icon';
 
+import { loadGeoJSON, loadBasemaps } from './layers';
+
 import iconNormal from '/icon-neutre.png';
 import iconActive from '/icon-select.png';
 
 import { showCard, hideCard } from './card';
 
-import { features } from './db';
+// import { features } from './db';
 
-let filteredFeatures = [];
+// let filteredFeatures = [];
 
-const transition = 16;
+// const size = { width, height: width * ratio };
 
-const basemap = new TileLayer({
-    source: new Stamen({ layer: 'watercolor' }),
-    maxZoom: transition,
-});
+// function icon(src, factor = 1) {
+//     return new Icon({
+//         anchor: [0.5, 1],
+//         height: size.height * factor,
+//         width: size.width * factor,
+//         anchorXUnits: 'fraction',
+//         anchorYUnits: 'fraction',
+//         src,
+//     });
+// }
 
-const osm = new TileLayer({
-    source: new OSM({
-        url: 'https://a.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png',
-    }),
-    minZoom: transition,
-});
+// const featureIconNormal = icon(iconNormal);
+// const featureIconHover = icon(iconNormal, 1.2);
+// const featureIconActive = icon(iconActive, 1.5);
 
-
-const size = { width : 29, height: 40 };
-
-function icon(src, factor = 1) {
-    return new Icon({
-        anchor: [0.5, 1],
-        height: size.height * factor,
-        width: size.width * factor,
-        anchorXUnits: 'fraction',
-        anchorYUnits: 'fraction',
-        src,
-    });
-}
-
-const featureIconNormal = icon(iconNormal);
-const featureIconHover = icon(iconNormal, 1.2);
-const featureIconActive = icon(iconActive, 1.5);
-
-const normal = new VectorLayer({
-    source: new VectorSource(),
-    style: new Style({
-        image: featureIconNormal,
-    })
-});
-
-const hovered = new VectorLayer({
-    source: new VectorSource(),
-    style: new Style({
-        image: featureIconHover
-    }),
-});
-
-const selected = new VectorLayer({
-    source: new VectorSource(),
-    style: new Style({
-        image: featureIconActive
-    }),
-});
+const baseLayers = loadBasemaps();
 
 const map = new Map({
-    layers: [osm, basemap, normal, hovered, selected],
+    layers: baseLayers,
     target: 'map',
     view: new View({
+        projection: 'EPSG:3857',
         center: fromLonLat([139.340, 38.822]),
         zoom: 5.5,
         minZoom: 5.5,
@@ -90,111 +60,112 @@ const map = new Map({
     }),
 });
 
+loadGeoJSON('/features.geojson').then(features => {
+    map.addLayer(features);
+});
+
 map.on('pointermove', (evt) => {
-    hovered.getSource().clear();
-    resetFeatures();
     const picked = map.getFeaturesAtPixel(evt.pixel);
     if (picked.length > 0) {
         const f = picked[0];
-        hovered.getSource().addFeature(f);
-        normal.getSource().removeFeature(f);
+        f.set('state', 'hover');
     }
 });
 
-/**
- * @param {Feature} feature
- */
-function select(feature) {
-    selected.getSource().addFeature(feature);
-    normal.getSource().removeFeature(feature);
+// /**
+//  * @param {Feature} feature
+//  */
+// function select(feature) {
+//     selected.getSource().addFeature(feature);
+//     normal.getSource().removeFeature(feature);
 
-    const tags = _.take(_.uniq(feature.get('tags')), 10);
+//     const tags = _.take(_.uniq(feature.get('tags')), 10);
 
-    const delay = 1000;
+//     const delay = 1000;
 
-    hideCard();
+//     hideCard();
 
-    showCard({
-        title: feature.get('name'),
-        url: feature.get('url'),
-        date: feature.get('date'),
-        tags,
-    });
+//     showCard({
+//         title: feature.get('name'),
+//         url: feature.get('url'),
+//         date: feature.get('date'),
+//         tags,
+//     });
 
-    setTimeout(() => {
-        const currentZoom = map.getView().getZoom();
+//     setTimeout(() => {
+//         const currentZoom = map.getView().getZoom();
 
-        map.getView().animate({
-            center: feature.get('position'),
-            duration: delay,
-            easing: easeOut,
-            zoom: currentZoom < 10 ? 10 : undefined,
-        });
-    }, 1);
-}
+//         map.getView().animate({
+//             center: feature.get('position'),
+//             duration: delay,
+//             easing: easeOut,
+//             zoom: currentZoom < 10 ? 10 : undefined,
+//         });
+//     }, 1);
+// }
 
-function clearSelection() {
-    selected.getSource().clear();
-    hideCard();
-}
+// function clearSelection() {
+//     selected.getSource().clear();
+//     hideCard();
+// }
 
-function resetFeatures() {
-    normal.getSource().clear();
-    normal.getSource().addFeatures(filteredFeatures);
-}
+// function resetFeatures() {
+//     normal.getSource().clear();
+//     normal.getSource().addFeatures(filteredFeatures);
+// }
 
-map.on('click', (evt) => {
-    selected.getSource().clear();
-    hovered.getSource().clear();
+// map.on('click', (evt) => {
+//     selected.getSource().clear();
+//     hovered.getSource().clear();
 
-    resetFeatures();
+//     resetFeatures();
 
-    const clicked = map.getFeaturesAtPixel(evt.pixel);
-    if (clicked.length > 0) {
-        const f = clicked[0];
-        select(f);
-    } else {
-        clearSelection();
-    }
-});
+//     const clicked = map.getFeaturesAtPixel(evt.pixel);
+//     if (clicked.length > 0) {
+//         const f = clicked[0];
+//         select(f);
+//     } else {
+//         clearSelection();
+//     }
+// });
 
 window.search = function (params) {
-    normal.getSource().clear();
+    // normal.getSource().clear();
 
-    if (params === "") {
-        filteredFeatures = [...features];
-        resetFeatures();
-        return;
-    }
+    // if (params === "") {
+    //     filteredFeatures = [...features];
+    //     resetFeatures();
+    //     return;
+    // }
 
-    const keywords = params
-        .split(',')
-        .map(w => w.trim())
-        .map(s => s.toLowerCase());
+    // const keywords = params
+    //     .split(',')
+    //     .map(w => w.trim())
+    //     .map(s => s.toLowerCase());
 
-    filteredFeatures.length = 0;
+    // filteredFeatures.length = 0;
 
-    function test(feature) {
-        const name = feature.get('name').toLowerCase();
-        if (keywords.some(kw => name.includes(kw))) {
-            return true;
-        }
+    // function test(feature) {
+    //     const name = feature.get('name').toLowerCase();
+    //     if (keywords.some(kw => name.includes(kw))) {
+    //         return true;
+    //     }
 
-        const tags = feature.get('tags');
-        if (tags.some(t => keywords.some(kw => t.toLowerCase().includes(kw)))) {
-            return true;
-        }
+    //     const tags = feature.get('tags');
+    //     if (tags.some(t => keywords.some(kw => t.toLowerCase().includes(kw)))) {
+    //         return true;
+    //     }
 
-        return false;
-    }
+    //     return false;
+    // }
 
-    for (const feature of features) {
-        if (feature && test(feature)) {
-            filteredFeatures.push(feature);
-        }
-    }
+    // for (const feature of features) {
+    //     if (feature && test(feature)) {
+    //         filteredFeatures.push(feature);
+    //     }
+    // }
 
-    resetFeatures();
+    // resetFeatures();
 }
 
 function addDate(num) {
@@ -213,17 +184,19 @@ function addDate(num) {
 }
 
 window.setDate = function (txt) {
-    source.clear();
+    // source.clear();
 
-    const date = Number.parseInt(txt);
-    addDate(date - 1);
-    addDate(date);
-    addDate(date + 1);
+    // const date = Number.parseInt(txt);
+    // addDate(date - 1);
+    // addDate(date);
+    // addDate(date + 1);
 }
 
-filteredFeatures = [...features];
-for (const f of features) {
-    if (f) {
-        normal.getSource().addFeature(f);
-    }
-}
+
+
+// filteredFeatures = [...features];
+// for (const f of features) {
+//     if (f) {
+//         normal.getSource().addFeature(f);
+//     }
+// }
